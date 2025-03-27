bafb13003e065e852e7eb92eed5fbd5e7b758964
Muhammad Haroon <m_haroon96@hotmail.com>
2025-03-27 02:02:19 -0400

    ported most UI

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ from PIL import Image
 import os
 import json
 import re
+from glob import glob
 
 def build_content_json(photo_dir, old_photos):
 
@@ -85,5 +86,16 @@ def main():
         json.dump(content, f, indent=4)
 
 
+def generate_ids():
+    json_files = glob('src/data/*.json')
+    for json_file in json_files:
+        with open(json_file) as f:
+            js = json.load(f)
+        for idx, entry in enumerate(js):
+            entry['id'] = idx
+        with open(json_file, 'w') as f:
+            json.dump(js, f, indent=2)
+
+
 if __name__ == '__main__':
-    main()
+    generate_ids()
--- src/content.config.ts
+++ src/content.config.ts
@@ -16,4 +16,57 @@ const publications = defineCollection({
     })
 });
 
-export const collections = { publications };
+const news = defineCollection({
+    loader: file("src/data/news.json"),
+    schema: z.object({
+        text: z.string(),
+        month: z.string(),
+        year: z.number(),
+    })
+});
+
+const talks = defineCollection({
+    loader: file("src/data/talks.json"),
+    schema: z.object({
+        title: z.string(),
+        year: z.number(),
+        venue: z.string(),
+        urls: z.array(z.object({
+            name: z.string(),
+            url: z.string()
+        }))
+    })
+});
+
+const podcasts = defineCollection({
+    loader: file("src/data/podcasts.json"),
+    schema: z.object({
+        title: z.string(),
+        year: z.number(),
+        host: z.string(),
+        urls: z.array(z.object({
+            name: z.string(),
+            url: z.string()
+        }))
+    })
+});
+
+const photos = defineCollection({
+    loader: file('src/data/photos.json'),
+    schema: z.object({
+        thumbnail: z.string(),
+        photo: z.string(),
+        description: z.string()
+    })
+});
+
+const misc = defineCollection({
+    loader: file('src/data/misc.json'),
+    schema: z.object({
+        thumbnail: z.string(),
+        photo: z.string(),
+        description: z.string()
+    })
+});
+
+export const collections = { publications, news, talks, podcasts, photos, misc };
